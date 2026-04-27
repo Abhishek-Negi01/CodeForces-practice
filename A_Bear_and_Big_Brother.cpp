813ccaec82669dbc1bb461a62854037229b95021
Abhishek Negi <negiabhishek75054@gmail.com>
2026-04-27 23:15:06 +0530

    solve problem no 791A and 617A

---
+++ A_Bear_and_Big_Brother.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int l, b;
+    cin >> l >> b;
+
+    int count = 0;
+
+    while (l <= b)
+    {
+        l = l * 3;
+        b = b * 2;
+
+        count++;
+    }
+
+    cout << count << endl;
+    return 0;
+}
