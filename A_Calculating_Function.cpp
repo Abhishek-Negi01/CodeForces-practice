e758118dac9603d972cc1fd624bb711f4f0389c1
Abhishek Negi <negiabhishek75054@gmail.com>
2026-05-13 23:09:36 +0530

    solved 486A

---
+++ A_Calculating_Function.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    long long n;
+    cin >> n;
+
+    if (n % 2 == 0)
+    {
+        cout << n / 2 << endl;
+    }
+    else
+    {
+        long long ans = -(n + 1) / 2;
+        cout << ans << endl;
+    }
+
+    return 0;
+}
